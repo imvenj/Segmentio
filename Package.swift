05ca4d4b7e1fb7ddf5348eb1a94a37aa9c91f542
Venj <ersaclarke@gmail.com>
2019-10-31 15:46:16 +0800

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "Segmentio",
+    platforms: [.iOS(.v10), .tvOS(.v10)],
     products: [
         .library(name: "Segmentio", targets: ["Segmentio"]),
     ],
-    dependencies: [],
     targets: [
         .target(
             name: "Segmentio",
-            path: "./Segmentio/Source"
+            path: "Segmentio/Source"
         )
     ]
 )
